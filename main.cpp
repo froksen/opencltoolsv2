9819df175f6c58cb05db9eafe9ae3e11c86ba7c3
Ole Holm Frandsen <froksen@gmail.com>
2016-09-19 17:33:29 +0200

    Tilføjet splashscreen

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <QApplication>
 //#include <QStyleFactory>
 //#include <QDebug>
+#include <QSplashScreen>
+#include <QPixmap>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
@@ -11,7 +14,18 @@ int main(int argc, char *argv[])
     //qDebug() << QStyleFactory::keys();
     //a.setStyle(QStyleFactory::create("WindowsVista"));
 
+    //Hoved vinduet
     MainWindow w;
-    w.show();
+
+    QSplashScreen *mSplashScreen = new QSplashScreen();
+    mSplashScreen->setPixmap(QPixmap(":/img/fingerFive.png"));
+    mSplashScreen->showMessage("Indlæser BLBLABLA");
+    a.processEvents(); //Sikre, at musen kan trykkes.
+
+    mSplashScreen->show(); //Viser splash
+    QTimer::singleShot(1200,&w,SLOT(show())); //Sikre, at splashscreenen vises mindst 1200 ms
+
+    mSplashScreen->finish(&w); //Når MainWindow er loaded, da lukkes splash automatisk.
+
     return a.exec();
 }
