b630366277b5602aae670882ea332437dfbc29d9
Ole Holm Frandsen <froksen@gmail.com>
2016-10-16 07:43:02 +0200

    Timer virker

--- tabs/stopwatch.cpp
+++ tabs/stopwatch.cpp
@@ -1,14 +1,116 @@
 #include "stopwatch.h"
 #include "ui_stopwatch.h"
+#include <QDebug>
+#include <QTime>
 
 StopWatch::StopWatch(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::StopWatch)
 {
     ui->setupUi(this);
+
+    QTimer *tickTimer = new QTimer(this);
+    connect(tickTimer,SIGNAL(timeout()),this,SLOT(updateTimer()));
+    tickTimer->setInterval(100);
+    setTickTimer(tickTimer);
+
+    setTime(0);
 }
 
 StopWatch::~StopWatch()
 {
     delete ui;
+}
+
+void StopWatch::updateTimer()
+{
+    //Lægger ny tid til
+    if(tickTimer()->isActive()){
+        quint64 nTime = time() + tickTimer()->interval();
+        setTime(nTime);
+    }
+
+    qDebug() << "updateTimer";
+    QString text = QTime(0,0,0).addMSecs(time()).toString(timeFormat());
+   ui->time->setText(text);
+}
+
+QTimer *StopWatch::tickTimer() const
+{
+    return _tickTimer;
+}
+
+void StopWatch::setTickTimer(QTimer *tickTimer)
+{
+    _tickTimer = tickTimer;
+}
+
+void StopWatch::on_StartStopButton_clicked()
+{
+    if(tickTimer()->isActive()){
+        tickTimer()->stop(); //Stopper timer
+        return;
+    }
+
+    updateFormat(); //Opdaterer tidsformatet
+    tickTimer()->start(); //Starter timer;
+}
+
+quint64 StopWatch::time() const
+{
+    return _time;
+}
+
+void StopWatch::setTime(const quint64 &time)
+{
+    _time = time;
+}
+
+void StopWatch::updateFormat()
+{
+    QString formatText;
+   //Timer
+    if(ui->showHours->isChecked())
+    {
+        formatText.append("HH:");
+    }
+
+    //Minutter
+    if(ui->showMinuts->isChecked())
+    {
+        formatText.append("mm:");
+    }
+
+
+    //Sekunder
+    formatText.append("ss");
+
+    //Sætter det nye format
+    setTimeFormat(formatText);
+}
+
+QString StopWatch::timeFormat() const
+{
+    return _timeFormat;
+}
+
+void StopWatch::setTimeFormat(const QString &timeFormat)
+{
+    _timeFormat = timeFormat;
+}
+
+void StopWatch::on_ResetButton_clicked()
+{
+    setTime(0);
+    updateTimer();
+}
+
+void StopWatch::on_showHours_clicked()
+{
+    updateFormat();
+}
+
+void StopWatch::on_showMinuts_clicked()
+{
+    updateFormat();
 }
--- tabs/stopwatch.h
+++ tabs/stopwatch.h
@@ -2,6 +2,7 @@
 #define STOPWATCH_H
 
 #include <QWidget>
+#include <QTimer>
 
 namespace Ui {
 class StopWatch;
@@ -15,8 +16,30 @@ public:
     explicit StopWatch(QWidget *parent = 0);
     ~StopWatch();
 
+    QString timeFormat() const;
+    void setTimeFormat(const QString &timeFormat);
+
+private slots:
+    void updateTimer();
+    void on_StartStopButton_clicked();
+    void on_ResetButton_clicked();
+    void on_showHours_clicked();
+    void on_showMinuts_clicked();
+
 private:
     Ui::StopWatch *ui;
+
+    QTimer *tickTimer() const;
+    void setTickTimer(QTimer *tickTimer);
+
+    quint64 time() const;
+    void setTime(const quint64 &time);
+
+    void updateFormat();
+
+    QString _timeFormat;
+    QTimer *_tickTimer;
+    quint64 _time;
 };
 
 #endif // STOPWATCH_H
